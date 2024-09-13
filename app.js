3ecb3798514d08ee1512ce950e0fd4c67065513a
Rod <128448506+barorod@users.noreply.github.com>
2024-09-13 13:05:29 +0800

    Add clearBoard functionality

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ board.style.cssText = `width: ${boardSide}px; height: ${boardSide}px`;
 function createDiv(divSize) {
   const sides = divSize * divSize;
   const cell = boardSide / divSize;
+
   for (let i = 0; i < sides; i++) {
     const div = document.createElement('div');
     div.style.cssText = `width: ${cell}px; height: ${cell}px`;
@@ -20,6 +21,15 @@ function createDiv(divSize) {
     board.appendChild(div);
 
     div.addEventListener('mouseover', handleHover);
+  }
+}
+
+function clearBoard() {
+  if (board.firstChild) {
+    const divs = document.querySelectorAll('#board > div');
+    divs.forEach((div) => div.remove());
+  } else {
+    return;
   }
 }
 
