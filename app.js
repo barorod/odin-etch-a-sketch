fef89d0234f2aeb509df3c5fe8eced47dcbbfae2
Rod <128448506+barorod@users.noreply.github.com>
2024-09-14 12:58:10 +0800

    Add custom color and random color functionality

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const divSize = 16;
 const board = document.querySelector('#board');
 const slider = document.querySelector('.slider');
 const sliderContainer = document.querySelector('.sliderContainer');
-const sliderValue = document.querySelector('.sliderContainer > p');
+const sliderValue = document.querySelector('#sliderVal');
+const select = document.querySelector('select');
+const inputs = document.querySelectorAll('.custom > input');
 
 sliderValue.innerText = `${slider.value}`;
-
 board.style.cssText = `width: ${boardSide}px; height: ${boardSide}px`;
+let color = 'black';
 
 function createDiv() {
   const cellTotal = parseInt(slider.value, 10);
-  const cellSize = board.clientWidth / parseInt(slider.value, 10);
+  const cellSize = board.clientWidth / cellTotal;
 
   for (let i = 0; i < cellTotal * cellTotal; i++) {
     const div = document.createElement('div');
@@ -29,15 +31,32 @@ function clearBoard() {
   if (board.firstChild) {
     const divs = document.querySelectorAll('#board > div');
     divs.forEach((div) => div.remove());
-  } else {
-    return;
   }
 }
 
 function handleHover() {
-  let color = getRandomColor();
-  this.style.backgroundColor = color;
+  this.style.backgroundColor = getColor();
 }
+
+function getColor() {
+  if (select.value === 'random') {
+    return getRandomColor();
+  } else if (select.value === 'black') {
+    return 'black';
+  } else if (select.value === 'custom') {
+    const r = inputs[0].value || 0;
+    const g = inputs[1].value || 0;
+    const b = inputs[2].value || 0;
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+  return 'black';
+}
+
+select.addEventListener('change', () => {
+  inputs.forEach((input) => {
+    input.disabled = select.value !== 'custom';
+  });
+});
 
 function getRandomColor() {
   const r = Math.floor(Math.random() * 256);
